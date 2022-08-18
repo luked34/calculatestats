68657ef97b85f703713e63f37ec7a6db34f2c21b
Luke <luked34@gmail.com>
2022-08-18 19:17:40 +0100

    john

--- src/main/java/org/example/CalculateStats.java
+++ src/main/java/org/example/CalculateStats.java
@@ -1,8 +1,16 @@
 package org.example;
 
+import java.util.Collection;
+
 public class CalculateStats {
 
     public String getHelloWorld() {
         return "Hello, Wojciech!";
+    }
+
+
+    public String result (Collection<Integer> dataset, StrategyInf strategyInf){
+        dataset.forEach(item -> strategyInf.apply(item));
+        return strategyInf.result();
     }
 }
